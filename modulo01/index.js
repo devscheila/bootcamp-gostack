14ffefdcb6393699035fba67f70a0a61a7bb945e
Scheila Stihler <sstihler@gmail.com>
2019-11-02 12:04:38 -0300

    Middlewares global e local

--- modulo01/index.js
+++ modulo01/index.js
@@ -32,20 +32,50 @@ server.get('/users/:id', (req, res) => {
 
 const users = ['Scheila', 'Noah', 'Maya', 'Barto'];
 
+// Middleware global de log
+server.use((req, res, next) => {
+  console.time('Request');
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd('Request');
+})
+
+// Middleware local - Verifica se o nome do usuário existe
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User name is required' });
+  }
+
+  return next();
+}
+
+// Middleware local - Verifica se o index do usuário existe
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index]
+  
+  if (!user) {
+    return res.status(400).json({ error: 'User does not exists' });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
 // Buscando todos os usuários do array
 server.get('/users', (req, res) => {
   return res.json(users);
 })
 
 // Buscando um usuário do array
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params; 
-
-  return res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  return res.json(req.user);
 })
 
 // Criar um usuário
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -54,7 +84,7 @@ server.post('/users', (req, res) => {
 })
 
 // Editar um usuário
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -64,13 +94,12 @@ server.put('/users/:index', (req, res) => {
 })
 
 // Deletar um usuário
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
 
   return res.send();
 })
-
 
 server.listen(3000);
