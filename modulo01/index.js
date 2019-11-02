311334c4232cfaedcfd7e2d47e2d9e6ea2e48229
Scheila Stihler <sstihler@gmail.com>
2019-11-02 11:35:39 -0300

    Instalação e configuração do nodemon

--- modulo01/index.js
+++ modulo01/index.js
@@ -17,11 +17,26 @@ server.get('/users', (req, res) => {
   return res.json({ message: `Hello ${nome}` });
 })
 
+/* 
 server.get('/users/:id', (req, res) => {
   // const id = req.params.id; -- ou usando desestruturação
   const { id } = req.params; 
 
   return res.json({ message: `Buscando o usuário ${id}` });
 })
+*/
+
+// CRUD - Create, Read, Update, Delete
+
+const users = ['Scheila', 'Noah', 'Maya', 'Barto'];
+
+// Buscando usuário do array
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params; 
+
+  return res.json(users[index]);
+})
+
+
 
 server.listen(3000);
