e0b1190ccdee401659334558888f17a1095ee52a
Scheila Stihler <sstihler@gmail.com>
2019-11-02 12:47:10 -0300

    Desafio01

---
+++ desafio01/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+// Middleware local - Verifica se o projeto já existe
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+// Middleware local - Registra o número de requisições
+function logRequests(req, res, next) {
+
+  console.count("Requests");
+
+  return next();
+}
+
+server.use(logRequests);
+
+// Listagem de todos os projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+// Cadastra um novo projeto
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+// Edita o título de um projeto
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+// Deleta um projeto
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+// Cadastra uma nova tarefa no projeto escolhido 
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3333);
